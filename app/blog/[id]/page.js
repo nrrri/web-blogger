7be5989b5df34ead5273e4955649319e3aeacc30
Your Name <you@example.com>
2024-04-01 20:07:57 -0400

    add blog

---
+++ app/blog/[id]/page.js
@@ -0,0 +1,38 @@
+import axios from 'axios'
+
+const fetchBlog = async (id) => {
+    try {
+        const response = await axios.get(`${process.env.STRAPI_BASE_URL}/api/blogs/${id}?populate=thumbnail,author`)
+
+        return response.data.data
+    } catch (e) {
+        console.log('error', e)
+        return []
+    }
+}
+
+export default async function Page({ params }) {
+    const blogId = params.id
+    const blog = await fetchBlog(blogId)
+    console.log(`${blog.attributes.author.data}`)
+    return (
+        <div className='mx-24 my-12'>
+            <div>
+                <img className='my-3' width="800px" src={`${process.env.STRAPI_BASE_URL}${blog.attributes.thumbnail.data.attributes.url}`} />
+            </div>
+            <div className='ml-12 mt-5'>
+                <div className='text-2xl font-bold uppercase mb-2'>
+                    {blog.attributes.title}
+                </div>
+                <div className='mb-3 ml-5'>
+                    {`"${blog.attributes.description}"`}
+                </div>
+                <div className='text-sm text-gray-400 italic ml-24'>
+                    Author by: {blog.attributes.author.data || "Anonymous"}
+                   
+                </div>
+            </div>
+
+        </div>
+    )
+}
